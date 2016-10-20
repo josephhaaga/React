e120d76020fbb64255e35b18a1b173f9d9a87da7
Joseph Haaga <joseph@Joseph-Dev-MBP-R-242.local>
2016-10-20 14:59:47 -0400

    comment form working

--- components.js
+++ components.js
@@ -43,23 +43,23 @@
 //  // 8888Y' 888888 'Y8888 88 88  88    888888 888888 888'   888888 888888      88   888888Y' 'Y88Y' 
 
 
-class Comment extends React.Component{
-	render() {
-		return (
-			<div className="comment">
-				<p className="comment-header">{this.props.author}</p>
-				<p className="comment-body">
-					{this.props.body}
-				</p>
-				<div className="comment-footer">
-					<a href="#" className="comment-footer-delete">
-						Delete Comment
-					</a>
-				</div>
-			</div>
-		)
-	}
-}
+// class Comment extends React.Component{
+// 	render() {
+// 		return (
+// 			<div className="comment">
+// 				<p className="comment-header">{this.props.author}</p>
+// 				<p className="comment-body">
+// 					{this.props.body}
+// 				</p>
+// 				<div className="comment-footer">
+// 					<a href="#" className="comment-footer-delete">
+// 						Delete Comment
+// 					</a>
+// 				</div>
+// 			</div>
+// 		)
+// 	}
+// }
 
 // class CommentBox extends React.Component{
 // 	render(){
@@ -141,11 +141,131 @@ class Comment extends React.Component{
  // 888888 88  88 8888Y'    888888 888888 888'   888888 888888      88   888888Y' 'Y88Y' 
 
 
+ // 8888o. 888888 .o8888 88 8888o.    88     888888 88  88 888888 88        888888 88  88 8888o. 888888 888888 
+ // 88  88 88     88     88 88  88    88     88     88  88 88     88          88   88  88 88  88 88     88     
+ // 8888Y' 8888   88  88 88 88  88    88     8888   88  88 8888   88          88   888888 8888Y' 8888   8888   
+ // 88  88 88     88  88 88 88  88    88     88     88 .8' 88     88          88   88  88 88  88 88     88     
+ // 8888Y' 888888 'Y8888 88 88  88    888888 888888 888'   888888 888888      88   88  88 88  88 888888 888888 
+
+
+class Comment extends React.Component{
+	constructor() {
+		super();
+		this.state = {isAbusive: false};
+	}
+	render() {
+		let commentBody;
+		if(!this.state.isAbusive){
+	      commentBody = this.props.body;
+	    }else{
+	    	commentBody = <em>Content marked as abuse</em>;
+	    }
+		return (
+			<div className="comment">
+				<p className="comment-header">{this.props.author}</p>
+				<p className="comment-body">
+					{commentBody}
+				</p>
+				<div className="comment-footer">
+					<a href="#" className="comment-footer-delete">
+						Delete Comment
+					</a>
+					<a href="#" onClick={this._toggleAbuse.bind(this)}>Report as Abuse</a>
+				</div>
+			</div>
+		)
+	}
+	_toggleAbuse(event){
+		event.preventDefault();
+	    this.setState({isAbusive: !this.state.isAbusive});
+	}
+
+}
+
+
+// class CommentBox extends React.Component{
+// 	constructor() {
+// 		super();
+// 		this.state = {
+// 			showComments: false
+// 		};
+// 	}
+
+// 	// this.setState({showComments:true});
+
+// 	render(){
+// 		const comments = this._getComments();
+// 		let commentNodes;
+// 		let buttonText = "Show comments";
+
+// 		if (this.state.showComments){
+// 			commentNodes = <div className="comment-list">{comments}</div>;
+// 			buttonText="Hide comments";
+// 		}
+// 		return (
+// 			<div className="comment-box">
+// 				<button onClick={this._handleClick.bind(this)}>{buttonText}</button>
+// 				<h3>Comments</h3>
+// 				<h4 className="comment-count">{this._getCommentsTitle(comments.length)}</h4>
+// 				{commentNodes}
+// 			</div>
+// 		);
+// 	}
+// 	_getCommentsTitle(commentCount) {
+// 		if(commentCount === 0){
+// 			return 'No comments yet';
+// 		}else if(commentCount === 1){
+// 			return '1 comment';
+// 		}else{
+// 			return `${commentCount} comments`;
+// 		}
+// 	}
+// 	// use underscore to distinguish custom methods from React methods
+// 	_getComments() { // returns array of dynamically-generated JSX elements
+// 		const commentList = [
+// 			{ id:1, author: 'Morgan McCircuit', body: 'Great picture!'},
+// 			{ id:2, author: 'Lorem Ipsum', body: 'Really great picture!'},
+// 			{ id:3, author: 'Bending Bender', body: 'Excellent stuff!'}
+// 		];
+// 		return commentList.map((comment)=>{
+// 			return (<Comment author={comment.author} body={comment.body} key={comment.id} />);
+// 				// pass comment's id as unique key; helps performance
+// 		});
+// 	}
+// 	_handleClick(){
+// 		this.setState({showComments:!this.state.showComments});
+// 	}
+
+// }
+
+
+
+
+
+// 888888 8888o. 8888o.    88     888888 88  88 888888 88        888888 88  88 8888o. 888888 888888 
+// 88     88  88 88  88    88     88     88  88 88     88          88   88  88 88  88 88     88     
+// 8888   88  88 88  88    88     8888   88  88 8888   88          88   888888 8888Y' 8888   8888   
+// 88     88  88 88  88    88     88     88 .8' 88     88          88   88  88 88  88 88     88     
+// 888888 88  88 8888Y'    888888 888888 888'   888888 888888      88   88  88 88  88 888888 888888 
+
+
+// 8888o. 888888 .o8888 88 8888o.    88     888888 88  88 888888 88        888888 .o88o. 88  88 8888o. 
+// 88  88 88     88     88 88  88    88     88     88  88 88     88        88     88  88 88  88 88  88 
+// 8888Y' 8888   88  88 88 88  88    88     8888   88  88 8888   88        8888   88  88 88  88 8888Y' 
+// 88  88 88     88  88 88 88  88    88     88     88 .8' 88     88        88     88  88 88  88 88  88 
+// 8888Y' 888888 'Y8888 88 88  88    888888 888888 888'   888888 888888    88     'Y88Y' 'Y88Y' 88  88 
+
+
 class CommentBox extends React.Component{
 	constructor() {
 		super();
 		this.state = {
-			showComments: false
+			showComments: false,
+			comments: [
+				{ id:1, author: 'Morgan McCircuit', body: 'Great picture!'},
+				{ id:2, author: 'Lorem Ipsum', body: 'Really great picture!'},
+				{ id:3, author: 'Bending Bender', body: 'Excellent stuff!'}
+			]
 		};
 	}
 
@@ -162,6 +282,7 @@ class CommentBox extends React.Component{
 		}
 		return (
 			<div className="comment-box">
+				<CommentForm addComment={this._addComment.bind(this)}/>
 				<button onClick={this._handleClick.bind(this)}>{buttonText}</button>
 				<h3>Comments</h3>
 				<h4 className="comment-count">{this._getCommentsTitle(comments.length)}</h4>
@@ -180,12 +301,12 @@ class CommentBox extends React.Component{
 	}
 	// use underscore to distinguish custom methods from React methods
 	_getComments() { // returns array of dynamically-generated JSX elements
-		const commentList = [
-			{ id:1, author: 'Morgan McCircuit', body: 'Great picture!'},
-			{ id:2, author: 'Lorem Ipsum', body: 'Really great picture!'},
-			{ id:3, author: 'Bending Bender', body: 'Excellent stuff!'}
-		];
-		return commentList.map((comment)=>{
+		// const commentList = [
+		// 	{ id:1, author: 'Morgan McCircuit', body: 'Great picture!'},
+		// 	{ id:2, author: 'Lorem Ipsum', body: 'Really great picture!'},
+		// 	{ id:3, author: 'Bending Bender', body: 'Excellent stuff!'}
+		// ];
+		return this.state.comments.map((comment)=>{
 			return (<Comment author={comment.author} body={comment.body} key={comment.id} />);
 				// pass comment's id as unique key; helps performance
 		});
@@ -193,7 +314,42 @@ class CommentBox extends React.Component{
 	_handleClick(){
 		this.setState({showComments:!this.state.showComments});
 	}
+	_addComment(author, body){
+		const comment = {
+			id: this.state.comments.length + 1,
+			author,
+			body
+		};
+		this.setState({ comments: this.state.comments.concat([comment]) });
+	}
+}
+
+
+class CommentForm extends React.Component{
+	render() {
+		return(
+			<form className="comment-name" onSubmit={this._handleSubmit.bind(this)}>
+				<label>Join the discussion</label>
+				<div className="comment-form-fields">
+					<input placeholder="Name:" ref={(input) => this._author = input} />
+					<textarea placeholder="Comment:" ref={(textarea) => this._body = textarea}></textarea>
+				</div>
+				<div className="comment-form-actions">
+					<button type="submit">Post Comment</button>
+				</div>
+			</form>
+		);
+	}
+	_handleSubmit(event){
+		event.preventDefault();
+		let author = this._author;
+		let body = this._body;
+		this.props.addComment(author.value, body.value);
+		// pass functions from parent component to child component
+	}
+
 }
 
 ReactDOM.render(<CommentBox />,document.getElementById('story-app'));
+
 
